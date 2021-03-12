4445893f108b10b40b063e652596252b5a17da22
Marquis Chamberlain <marquischamberlain@gmail.com>
2021-03-11 22:48:05 -0500

    adding optional driver

---
+++ projects/assignment_2/item_package/driver.java
@@ -0,0 +1,11 @@
+package assignment_2.item_package;
+
+public class driver {
+    public static void main (String args []) {
+        /*
+            This can be where you attempt to test constructing your objects!
+
+            Additionally, you can create a main method in your item class if you so choose.
+         */
+    }
+}
