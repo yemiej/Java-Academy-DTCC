1d879fcb48b9b0383d25bb3e892405ecb9411168
Marquis Chamberlain <marquischamberlain@gmail.com>
2021-03-25 00:46:36 -0400

    adding echo server

--- projects/assignment_4/com/dtcc/Main.java
+++ projects/assignment_4/com/dtcc/Main.java
@@ -1,11 +1,50 @@
 package assignment_4.com.dtcc;
 
+import java.net.*;
+import java.io.*;
+import java.util.Arrays;
+
 public class Main {
-    public static void main (String args[]){
-        int i = 0;
-        while(i < 10) {
-            System.out.println(String.format("[%d]", i));
-            i++;
+    public static void main (String args[]) throws Exception {
+        String host = "127.0.0.1";
+        short port = 8080;
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(port, 0, InetAddress.getByName(host));
+
+            System.err.println("Server listening on " + host + ":" + port + "\n");
+            int read = 0;
+            byte[] buffer = new byte[8192];
+
+            while(true) {
+                PrintWriter out = null;
+                try {
+                    Socket client = server.accept();
+                    System.out.println("Connection accepted from " + client.getRemoteSocketAddress());
+                    out = new PrintWriter(client.getOutputStream(), true);
+                    InputStream in = client.getInputStream();
+                    System.out.write(buffer, 0, (read = in.read(buffer)));
+                    System.out.println("");
+                    String output = "Hello Moto";
+                    out.println("HTTP/1.1 200 OK " + output.length() + " " + output);
+                    out.close();
+                    in.close();
+                    client.close();
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+                finally {
+                    if (out != null) {
+                        out.flush();
+                    }
+                }
+            }
+        }
+        finally {
+            System.out.println("Closing");
+            if(server != null)
+                server.close();
         }
     }
 }
