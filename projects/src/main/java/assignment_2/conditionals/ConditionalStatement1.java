4f944cc4efdb8bfd400860c9e884c9a3882bc84a
yemiej <oyeyemi.jimoh@gmail.com>
2021-06-19 14:30:18 -0700

    Conditional Statement 1 updated

--- projects/src/main/java/assignment_2/conditionals/ConditionalStatement1.java
+++ projects/src/main/java/assignment_2/conditionals/ConditionalStatement1.java
@@ -6,7 +6,7 @@ public class ConditionalStatement1 {
         // Replace false with a statement that will resolve to true, using the int value
         // This will yield the opposite, false, but an example is value != 100
         // Remember you want to win ;)
-        if(false) {
+        if(value != 1) {
             System.out.println("You Win");
         } else {
             System.out.println("You Lose");
