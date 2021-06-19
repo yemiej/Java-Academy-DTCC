a57042e22ebb9ec0b085c08deb04b59fb17f0c9f
yemiej <oyeyemi.jimoh@gmail.com>
2021-06-19 13:42:42 -0700

    Change message to Hello World

--- projects/src/main/java/assignment_1/HelloWorld.java
+++ projects/src/main/java/assignment_1/HelloWorld.java
@@ -1,7 +1,7 @@
 package assignment_1;
 
 public class HelloWorld {
-    final private static String statement = "Marquis Chafdsfsf AA";
+    final private static String statement = "Hello World";
     public static void main (String[] args)  {
         System.out.println(statement);
     }
