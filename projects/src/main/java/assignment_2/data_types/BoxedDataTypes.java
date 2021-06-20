2a58699e3c483bf5f4675831278b0281e67e78b4
yemiej <oyeyemi.jimoh@gmail.com>
2021-06-20 16:33:33 -0700

    Initial attempt for the 3 classes

--- projects/src/main/java/assignment_2/data_types/BoxedDataTypes.java
+++ projects/src/main/java/assignment_2/data_types/BoxedDataTypes.java
@@ -9,5 +9,10 @@ public class BoxedDataTypes {
         Integer i_1 = 0;
         // versus
         int i_2 = 0;
+
+        System.out.println( "Value of Integer i_1 = " +i_1);
+        System.out.println( "Value of int i_2 = " + i_2);
+        System.out.println( "Is i_1 == i_2?     The answer is " + (i_1 == i_2));
+        System.out.println( "Is i_1 equals i_2? The answer is " + (i_1.equals( i_2)));
     }
 }
